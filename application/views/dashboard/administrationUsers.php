bcc86606f72ca5d9c04410e6da6aa25abcb069f1
josepablo <rmpros2@gmail.com>
2020-06-08 09:24:38 -0500

    Administration User view styles addition.

--- application/views/dashboard/administrationUsers.php
+++ application/views/dashboard/administrationUsers.php
@@ -5,27 +5,29 @@
 
 <div class="admin-select pric-sclr1">
 	<ul>
-		<li><a class="popup-with-zoom-anim" href="<?php echo base_url() . 'billing/listUsers'?>">List
-			Users</a></li> 
+		<li><a class="popup-with-zoom-anim" 
+			href="<?php echo base_url() . 'billing/listUsers'?>">List
+			Users</a> 
 			<a class="popup-with-zoom-anim"
 			href="<?php echo base_url() . 'billing/listUsersInTrial'?>">List
-			Users In Trial</a></li>
-		<li><a class="popup-with-zoom-anim"
+			Users In Trial</a>
+			<a class="popup-with-zoom-anim"
 			href="<?php echo base_url() . 'billing/listUsersInPlan'?>">List
-			Users In Plan</a> </li>
-		<li><a class="popup-with-zoom-anim"
+			Users In Plan</a>
+			<a class="popup-with-zoom-anim"
 			href="<?php echo base_url() . 'billing/listIdleUsers'?>">List
 			Idle Users</a></li>
+
 		<li><a class="popup-with-zoom-anim"
 			href="<?php echo base_url() . 'billing/listMonthyProfitPerPlan'?>">Monthy Profit
-			Per Plan</a></li>
-		<li><a class="popup-with-zoom-anim"
+			Per Plan</a>
+			<a class="popup-with-zoom-anim"
 			href="<?php echo base_url() . 'billing/listAnnualProfitPerPlan'?>">Annual Profit
 			Per Plan</a></li> 
 		<li><a class="popup-with-zoom-anim"
 			href="<?php echo base_url() . 'billing/listMonthlyBilling'?>">Monthly
-			Billing</a></li> 
-		<li><a class="popup-with-zoom-anim"
+			Billing</a> 
+			<a class="popup-with-zoom-anim"
 			href="<?php echo base_url() . 'billing/listAnnualBilling'?>">Annual
 			Billing</a></li>
 	</ul>
